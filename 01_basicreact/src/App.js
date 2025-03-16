36cab6ff2027cc5b877e4cde734bda342a5da60b
rajyadav <vector36razz@gmail.com>
2025-03-16 22:28:40 +0530

    basic react folder added

---
+++ 01_basicreact/src/App.js
@@ -0,0 +1,12 @@
+import Chai from "./Chai";
+
+function App() {
+  return (
+    <div>
+      <h3>Welcome to React</h3>
+      <h1>This is h1</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ 01_basicreact/src/Chai.js
@@ -0,0 +1,8 @@
+
+function Chai() {
+    return (
+        <h3>Chai and React</h3>
+    )
+}
+
+export default Chai;
