f2aeaf20d58fac7632305d5aa8e9176c9c2b322e
rajyadav <vector36razz@gmail.com>
2025-03-16 11:06:04 +0530

    Why React is needed

---
+++ Why-React/script.js
@@ -0,0 +1,6 @@
+
+const leftBasket = document.getElementsByClassName(basket1)
+const rightBasket = document.getElementsByClassName(basket2)
+const leftButton = document.getElementById(left-btn);
+const rightButton = document.getElementById(right-btn);
+
