78349db3c55643fa2598fa31934d008815bb21f8
rajyadav <vector36razz@gmail.com>
2025-03-17 00:16:46 +0530

    main.jsx updated

--- 01_vitereact/src/main.jsx
+++ 01_vitereact/src/main.jsx
@@ -3,8 +3,33 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import Chai from "./chai.jsx";
 
+function FunApp(){
+  return(
+    <h3>function inside main</h3>
+  )
+}
+// const ReactElement = {
+//   type: 'a',
+//   props: {
+//       href: "https://www.google.com",
+//       target: '_blank'
+//   },
+//   children: 'Click me to visit google'
+// }  //it will not work cuz of syntax error
+
+const AnotherReactElement = (
+  <a href="https://www.google.com" target='_blank'>Visit google</a>
+)
+
+//a function according to react
+const ReactElement = (
+  'a', //tags
+  {href: "https://www.google.com", target: '_blank'},
+  'Visit Google Official'
+)
+
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <Chai />
-  </StrictMode>
+  
+    <App />
+  
 );
