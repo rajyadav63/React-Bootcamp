124757f721e67d316b54673352b5f942d3fad809
rajyadav <vector36razz@gmail.com>
2025-03-16 11:58:42 +0530

    Basket counter code added

--- Why-React/script.js
+++ Why-React/script.js
@@ -1,6 +1,40 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const leftBasketCount = document.querySelector(".basket1 span");
+  const rightBasketCount = document.querySelector(".basket2 span");
+  const leftButton = document.getElementById("left-btn");
+  const rightButton = document.getElementById("right-btn");
 
-const leftBasket = document.getElementsByClassName(basket1)
-const rightBasket = document.getElementsByClassName(basket2)
-const leftButton = document.getElementById(left-btn);
-const rightButton = document.getElementById(right-btn);
+  const totoalAppleCount = 10;
 
+  let rightBasketAppleCount = 0;
+  let leftBasketAppleCount = totoalAppleCount - rightBasketAppleCount;
+
+  leftBasketCount.innerText = leftBasketAppleCount;
+  rightBasketCount.innerText = rightBasketAppleCount;
+
+  rightButton.addEventListener("click", () => {
+    // console.log(leftBasketAppleCount);
+    // console.log(rightBasketAppleCount);
+    if(leftBasketAppleCount > 0){
+        leftBasketAppleCount--;
+        leftBasketCount.innerText = leftBasketAppleCount;
+        rightBasketAppleCount++;
+        rightBasketCount.innerText = rightBasketAppleCount;
+    }
+    if(rightBasketAppleCount === 10){
+        alert("Basket is full")
+    }
+  });
+
+  leftButton.addEventListener("click", () => {
+    if(rightBasketAppleCount > 0){
+        leftBasketAppleCount++;
+        leftBasketCount.innerText = leftBasketAppleCount;
+        rightBasketAppleCount--;
+        rightBasketCount.innerText = rightBasketAppleCount;
+    }
+    if(leftBasketAppleCount === 10){
+        alert("Basket is full")
+    }
+  });
+});
