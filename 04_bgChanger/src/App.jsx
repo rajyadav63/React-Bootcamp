d5a402d4b272533e623e1446daef4a2ea0ff4a9a
rajyadav <vector36razz@gmail.com>
2025-03-19 11:42:59 +0530

    crimson color  added

--- 04_bgChanger/src/App.jsx
+++ 04_bgChanger/src/App.jsx
@@ -6,57 +6,62 @@ function App() {
 
   return (
     <div className="w-full h-screen duration-150 flex justify-center"
-    style={{backgroundColor: color}}>
+      style={{ backgroundColor: color }}>
       <div className='fixed flex flex-wrap justify-center items-center bottom-10'>
         <div className='flex flex-wrap justify-center items-center gap-3 bg-amber-50 px-3 py-1.5 rounded-xl'>
 
           <button className='text-white px-3 text-center py-1 rounded-2xl cursor-pointer'
-          style={{backgroundColor: "blue"}}
-          onClick={()=>{setColor("blue")}}
+            style={{ backgroundColor: "blue" }}
+            onClick={() => { setColor("blue") }}
           >Blue</button>
 
           <button className='text-white px-3 text-center py-1 rounded-2xl cursor-pointer'
-          style={{backgroundColor: "green"}}
-          onClick={()=>{setColor("green")}}
+            style={{ backgroundColor: "green" }}
+            onClick={() => { setColor("green") }}
           >Green</button>
 
           <button className='text-white px-3 text-center py-1 rounded-2xl cursor-pointer'
-          style={{backgroundColor: "red"}}
-          onClick={()=>{setColor("red")}}
-          >Red</button>
+            style={{ backgroundColor: "crimson" }}
+            onClick={() => { setColor("crimson") }}
+          >Crimson</button>
           <button className='text-white px-3 text-center py-1 rounded-2xl cursor-pointer'
-          style={{backgroundColor: "purple"}}
-          onClick={()=>{setColor("purple")}}
+            style={{ backgroundColor: "purple" }}
+            onClick={() => { setColor("purple") }}
           >Purple</button>
           <button className='text-black px-3 text-center py-1 rounded-2xl cursor-pointer'
-          style={{backgroundColor: "aquamarine"}}
-          onClick={()=>{setColor("aquamarine")}}
+            style={{ backgroundColor: "aquamarine" }}
+            onClick={() => { setColor("aquamarine") }}
           >Aquamarine</button>
 
           <button className='text-white px-3 text-center py-1 rounded-2xl cursor-pointer'
-          style={{backgroundColor: "olive"}}
-          onClick={()=>{setColor("olive")}}
+            style={{ backgroundColor: "olive" }}
+            onClick={() => { setColor("olive") }}
           >Olive</button>
 
           <button className='text-black px-3 text-center py-1 rounded-2xl cursor-pointer'
-          style={{backgroundColor: "yellow"}}
-          onClick={()=>{setColor("yellow")}}
+            style={{ backgroundColor: "yellow" }}
+            onClick={() => { setColor("yellow") }}
           >Yellow</button>
 
           <button className='text-white px-3 text-center py-1 rounded-2xl cursor-pointer'
-          style={{backgroundColor: "black"}}
-          onClick={()=>{setColor("black")}}
+            style={{ backgroundColor: "black" }}
+            onClick={() => { setColor("black") }}
           >Black</button>
 
           <button className='text-black px-3 text-center py-1 rounded-2xl cursor-pointer'
-          style={{backgroundColor: "white"}}
-          onClick={()=>{setColor("white")}}
+            style={{ backgroundColor: "white" }}
+            onClick={() => { setColor("white") }}
           >White</button>
 
           <button className='text-white px-3 text-center py-1 rounded-2xl cursor-pointer'
-          style={{backgroundColor: "pink"}}
-          onClick={()=>{setColor("pink")}}
+            style={{ backgroundColor: "pink" }}
+            onClick={() => { setColor("pink") }}
           >Pink</button>
+
+          <button className='text-white px-3 text-center py-1 rounded-2xl cursor-pointer'
+            style={{ backgroundColor: "red" }}
+            onClick={() => { setColor("red") }}
+          >Red</button>
 
         </div>
       </div>
