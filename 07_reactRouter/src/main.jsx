f1931e638a4f6fc08724b575ed4263e6e353dc95
rajyadav <vector36razz@gmail.com>
2025-03-30 01:15:58 +0530

    main file updated

--- 07_reactRouter/src/main.jsx
+++ 07_reactRouter/src/main.jsx
@@ -8,6 +8,7 @@ import Layout from './Layout.jsx'
 import Header from './components/Header/Header.jsx'
 import About from './components/About/About.jsx';
 import Contact from './components/Contact/Contact.jsx'
+import User from './components/User/User.jsx'
 
 
 // const router = createBrowserRouter([
@@ -41,6 +42,7 @@ const router = createBrowserRouter(
       <Route path='' element={<Home />}/>
       <Route path='about' element={<About />}/>
       <Route path='contact' element={<Contact />}/>
+      <Route path='user/:userid' element={<User />}/>
     </Route>
   )
 )
