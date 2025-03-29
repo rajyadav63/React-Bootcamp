edaddf37c311b26c82bf1f10406b56fc8705644f
rajyadav <vector36razz@gmail.com>
2025-03-30 01:01:10 +0530

    About components added

--- 07_reactRouter/src/main.jsx
+++ 07_reactRouter/src/main.jsx
@@ -2,10 +2,52 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { RouterProvider } from 'react-router'
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router'
+import Home from './components/Home/Home.jsx'
+import Layout from './Layout.jsx'
+import Header from './components/Header/Header.jsx'
+import About from './components/About/About.jsx';
+import Contact from './components/Contact/Contact.jsx'
+
+
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <Layout />,
+//     children: [
+//       {
+//         path: "",
+//         element: <Home />
+//       },
+//       {
+//         path: "about",
+//         element: <About />
+//       },
+//       {
+//         path: "header",
+//         element: <Header />
+//       },
+//       {
+//         path: "contact",
+//         element: <Contact />
+//       }
+//     ]
+//   }
+// ])
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Layout />}>
+      <Route path='' element={<Home />}/>
+      <Route path='about' element={<About />}/>
+      <Route path='contact' element={<Contact />}/>
+    </Route>
+  )
+)
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider />
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
