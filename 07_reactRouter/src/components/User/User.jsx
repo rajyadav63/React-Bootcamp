0148d66ac823efa0f9e967e97ff2fd0bc43d0d79
rajyadav <vector36razz@gmail.com>
2025-03-30 11:36:49 +0530

    User file updated

--- 07_reactRouter/src/components/User/User.jsx
+++ 07_reactRouter/src/components/User/User.jsx
@@ -5,9 +5,7 @@ import { useParams } from 'react-router'
 function User() {
     const {userid} = useParams()
   return (
-    <>
-      User: {userid}
-    </>
+    <div className='bg-orange-400 py-3 text-2xl'>User: {userid}</div>
   )
 }
 
