6558d7636f44440c81e99776e34e66f8a0791f1e
rajyadav <vector36razz@gmail.com>
2025-04-01 14:50:49 +0530

    theme context added

--- 09_themeSwitcher/src/App.jsx
+++ 09_themeSwitcher/src/App.jsx
@@ -1,19 +1,37 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
+import { ThemeProvider } from "./contexts/Theme";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [themeMode, setThemeMode] = useState('light');
+
+   function lightTheme() {
+    setThemeMode('light')
+  }
+   function darkTheme() {
+    setThemeMode('dark')
+  }
+
+  useEffect(() => {
+    document.querySelector('html').classList.remove('light', 'dark')
+    document.querySelector('html').classList.add(themeMode)
+  }, [themeMode])
+  
 
   return (
-    <div className="flex flex-wrap min-h-screen items-center">
-      <div className="w-full">
-        <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
-          {/* themeBtn*/}
+    <ThemeProvider value={{themeMode, lightTheme, darkTheme}}>
+      <div className="flex flex-wrap min-h-screen items-center">
+        <div className="w-full">
+          <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+            {/* themeBtn*/}
+          </div>
+          <div>
+            <h1 className="bg-blue-400 p-3 rounded-2xl">Theme Switcher</h1>
+          </div>
+          <div className="w-full max-w-sm mx-auto">{/* Card*/}</div>
         </div>
-
-        <div className="w-full max-w-sm mx-auto">{/* Card*/}</div>
       </div>
-    </div>
+    </ThemeProvider>
   );
 }
 
