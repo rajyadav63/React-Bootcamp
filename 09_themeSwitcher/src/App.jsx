7f4848209d026157beed22edac4e4f00b817272d
rajyadav <vector36razz@gmail.com>
2025-04-01 16:07:17 +0530

    themeBtn and Card added but toggle not working

--- 09_themeSwitcher/src/App.jsx
+++ 09_themeSwitcher/src/App.jsx
@@ -1,34 +1,37 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 import { ThemeProvider } from "./contexts/Theme";
+import ThemeBtn from "./components/ThemeBtn";
+import Card from "./components/Card";
 
 function App() {
-  const [themeMode, setThemeMode] = useState('light');
+  const [themeMode, setThemeMode] = useState("light");
 
-   function lightTheme() {
-    setThemeMode('light')
+  function lightTheme() {
+    setThemeMode("light");
   }
-   function darkTheme() {
-    setThemeMode('dark')
+  function darkTheme() {
+    setThemeMode("dark");
   }
 
   useEffect(() => {
-    document.querySelector('html').classList.remove('light', 'dark')
-    document.querySelector('html').classList.add(themeMode)
-  }, [themeMode])
-  
+    //remove whatever is there
+    document.querySelector("html").classList.remove("light", "dark");
+    //add according to theme
+    document.querySelector("html").classList.add(themeMode);
+  }, [themeMode]);
 
   return (
-    <ThemeProvider value={{themeMode, lightTheme, darkTheme}}>
-      <div className="flex flex-wrap min-h-screen items-center">
+    <ThemeProvider value={{ themeMode, lightTheme, darkTheme }}>
+      <div className="flex flex-wrap min-h-screen items-center bg-white dark:bg-gray-900">
         <div className="w-full">
           <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
-            {/* themeBtn*/}
+            <ThemeBtn />
           </div>
-          <div>
-            <h1 className="bg-blue-400 p-3 rounded-2xl">Theme Switcher</h1>
+
+          <div className="w-full max-w-sm mx-auto">
+            <Card />
           </div>
-          <div className="w-full max-w-sm mx-auto">{/* Card*/}</div>
         </div>
       </div>
     </ThemeProvider>
